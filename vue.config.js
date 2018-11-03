ff381caeb39d9465b6af060004bfb24b5b63b4b7
Stephen Toms <stoms@DIGI5.local>
2018-11-03 02:07:05 -0400

    ignored lint on vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,24 @@
-const path = require('path')
-const webpack = require('webpack')
+/* eslint-disable */
+
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   configureWebpack: {
     resolve: {
-      extensions: ['.js'],
+      extensions: [".js"],
       alias: {
-        'jquery': 'jquery/dist/jquery.slim.js',
+        jquery: "jquery/dist/jquery.slim.js"
       }
     },
     plugins: [
       new webpack.ProvidePlugin({
-        '$': 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default'],
-        'Util': "exports-loader?Util!bootstrap/js/dist/util"
-      }),
+        $: "jquery",
+        jQuery: "jquery",
+        Popper: ["popper.js", "default"],
+        Util: "exports-loader?Util!bootstrap/js/dist/util"
+      })
       // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     ]
   }
-}
+};
