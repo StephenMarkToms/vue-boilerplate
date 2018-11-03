668b004d430974fab2ff744d00359cd148b859a9
Stephen Toms <stoms@DIGI5.local>
2018-11-03 01:58:16 -0400

    added config

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/* eslint-disable */
+
+// import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap';
+
+// import plugins individually - require exports-loader
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/tooltip';
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/scss/bootstrap.scss';
+
+// jquery
+global.$ = $;
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
